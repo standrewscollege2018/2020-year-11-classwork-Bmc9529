623dfebc360b9ff7fe7e1eec5af1ec3178bb004a
Bmc9529 <bmc9529@stacmail.net>
2020-02-04 10:31:26 +1300

    Create Variable intro.py

---
+++ Variable intro.py
@@ -0,0 +1,29 @@
+""" This program demonstrates how to use variables to store information and how to print out the value of a varialbe """
+
+# variables should have appriate names
+
+name = "John"
+
+# if there are mulitple words in the variable name, use underscores
+
+last_name = "Smith"
+
+print("Hello",name, last_name)
+
+
+age = 15
+# above is stored as a number so if you want it to work do below
+print("You are", age, "years old")
+#below is wrong
+#print("You're" + age + "years old")
+
+""" when getting input, use input() function"""
+""" we need to set a variable to store the input"""
+fav_animal = input("what is your favourite animal?")
+print("Wrong!", fav_animal, "is nothing compared to the majestic reinturtle") 
+
+print("to have another say enter your credit card details")
+scam = input("Enter your code on the front, Exp. date and the 3 numbers on the back")
+
+
+      
