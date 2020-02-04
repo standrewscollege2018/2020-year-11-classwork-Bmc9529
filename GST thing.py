0d26c16918f69aad4c49f897057bd68bd954cbd1
Bmc9529 <bmc9529@stacmail.net>
2020-02-05 12:26:48 +1300

    Update GST thing.py

--- GST thing.py
+++ GST thing.py
@@ -1,8 +1,10 @@
-#if their input is yes it will go to remove gst otherwise add
+""" This program gets an item name and price, then adds GST to the price. """
+
+#if their input is yes it will go to remove gst otherwise add it
 inpt = input("Do you want to remove GST: ")
 
-
-if(inpt = yes):
+#when testing a condition use == not =
+if inpt == "yes":
    item_name = input("Item name?: ")
    item_price = float(input("Item price?: "))
    print("The", item_name, "will cost","$", item_price / 1.15, "excluding GST")
