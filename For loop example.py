7f1384c81eee191cd116a32583d3ea4167dbdaeb
Bmc9529 <bmc9529@stacmail.net>
2020-02-13 12:22:54 +1300

    Update For loop example.py

--- For loop example.py
+++ For loop example.py
@@ -1,7 +1,7 @@
 """ for loop example, for loops run up to but not the final number"""
 i = 1
 #in for loops we set a start, enging and increment valu
-for i in range(1,101):
+for i in range(1,1001):
     if i % 3 == 0 and i % 5 == 0:
         print("fizzbuzz")
     elif i % 3 == 0 and not i % 5 == 0:
