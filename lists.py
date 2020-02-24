b74f19507b0066c7d2157a89cf2af97183901cf6
Bmc9529 <bmc9529@stacmail.net>
2020-02-25 10:03:27 +1300

    Update lists.py

--- lists.py
+++ lists.py
@@ -5,14 +5,17 @@ loop = True
 numbers = []
 
 while loop == True:
-        A = float(input("Enter a positive number:\n")) 
-        if A == -1:
-                loop = False
-                print("You entered these numbers")
-                for i in range(len(numbers)):
-                        print(numbers[i])
-                print("The mean is", sum(numbers) / len(numbers))
-        elif A < 0:
-                print()
-        else:
-                numbers.append(A)
+      try:
+            A = float(input("Enter a positive number:\n")) 
+            if A == -1:
+                    loop = False
+                    print("You entered these numbers")
+                    for i in range(len(numbers)):
+                            print(numbers[i])
+                    print("The mean is", sum(numbers) / len(numbers))
+            elif A < 0:
+                    print()
+            else:
+                    numbers.append(A)
+      except:
+            print("Please dear lord put in a valid number")
