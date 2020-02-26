db4ad47a22d6b9e5b1ab500085f58095f92e6230
Bmc9529 <bmc9529@stacmail.net>
2020-02-27 11:40:04 +1300

    rental cars

---
+++ Rental cars.py
@@ -0,0 +1,15 @@
+"""Rental cars tasks using certain list functions and tells people if cars are booked and stuff like that"""
+cars = []
+car = ["Renault", 15000]
+cars.append(car)
+print(cars)
+
+
+
+#starts the program
+cars = [["Suzuki Van", 2, False], ["Toyota Corlla", 4, False], ["Honda CRV", 4, False], ["Suzuki Swift", 4, False], ["Mitsibishi Airtrek", 4, False], ["Nissan DC Ute"], ["Toyota Previa", 7, False], ["Toyota High Ace", 12, False], ["Toyota High Ace", 12, False]]
+loop = False
+
+#starts the gui
+seats = int(input("Please enter the number of seats required (type -1 to quit):\n"))
+
