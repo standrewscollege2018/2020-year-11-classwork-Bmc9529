2c0edfbce2f57bfa35d4359d40fe344eab575c90
Bmc9529 <bmc9529@stacmail.net>
2020-03-05 11:05:50 +1300

    Create Check for symbols.py

---
+++ Check for symbols.py
@@ -0,0 +1,18 @@
+chars = set('0123456789$,!@#%^&*()')
+
+names = []
+
+keep_asking = True
+
+while keep_asking == True:
+        name = input("Enter your name: ")
+        if name == "":
+                print("End a name")
+        elif any((c in chars) for c in name):
+                print("Enter a valid name")
+        elif name.lower() == "end":
+                keep_asking = False
+        else:
+                names.append(name)
+
+print(names)
